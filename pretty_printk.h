4f7d072d9474d79e4f8ae180280fce552f69eb6c
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-04-27 16:24:42 +0200

    Adding skeleton module for demo and a skeleton header file for macros

---
+++ pretty_printk.h
@@ -0,0 +1,32 @@
+/*
+ * pretty-printk::Macro collection
+ * <https://github.com/tpiekarski/pretty-printk>
+ * ---
+ * Copyright 2020 Thomas Piekarski <t.piekarski@deloquencia.de>
+ *
+ * This file is part of pretty-printk - a pretty way to print to 
+ * the Linux Kernel Ring Buffer
+ * 
+ * pretty-printk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * pretty-printk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with pretty-printk. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef _LINUX_PRETTY_PRINTK_H
+#define _LINUX_PRETTY_PRINTK_H
+
+// todo: print a lot of things in (more or less) pretty way
+
+#define _LINUX_PP_MESSAGE "This is a demo module for pretty printk-ing"
+
+#endif
